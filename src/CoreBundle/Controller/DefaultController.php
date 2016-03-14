a9a70493e75705a1bf303f140afdb5bd603dd88a
Etienne DEBARD <etienne.debard@etu.univ-lyon1.fr>
2016-03-14 18:01:47 +0100

    ajout routes

--- src/CoreBundle/Controller/DefaultController.php
+++ src/CoreBundle/Controller/DefaultController.php
@@ -3,6 +3,10 @@
 namespace CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use CoreBundle\Entity\Article;
+use CoreBundle\Form\ArticleType;
 
 class DefaultController extends Controller
 {
@@ -10,4 +14,44 @@ class DefaultController extends Controller
     {
         return $this->render('CoreBundle:Default:index.html.twig');
     }
+
+    public function categoryAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $listArticles = $em->getRepository('CoreBundle:Article') ->getArticleWithCategoryId($id);
+        $category = $em->getRepository('CoreBundle:Category')->find($id);
+
+        // Si l'annonce n'existe pas, on affiche une erreur 404
+        if ($category == null) {
+          throw $this->createNotFoundException("La categorie d'id ".$id." n'existe pas.");
+        }
+
+        return $this->render('CoreBundle:category.html.twig', array('category' => $category,
+            'articles' => $listArticles));
+    }
+
+    public function articleAction($id)
+    {
+        return $this->render('CoreBundle:article.html.twig');
+    }
+
+    public function addAction(Request $request)
+      {
+        $article = new Article();
+        $form = $this->get('form.factory')->create(ArticleType::class, $article);
+
+        if ($form->handleRequest($request)->isValid()) {
+          $em = $this->getDoctrine()->getManager();
+          $em->persist($article);
+          $em->flush();
+
+          $request->getSession()->getFlashBag()->add('notice', 'Article bien enregistré.');
+
+          return $this->redirect($this->generateUrl('core_article', array('id' => $article->getId())));
+        }
+
+        return $this->render('CoreBundle:add.html.twig', array(
+          'form' => $form->createView(),
+        ));
+      }
 }
---
+++ src/CoreBundle/Form/ArticleType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ArticleType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('content')
+            ->add('author')
+            ->add('note')
+            ->add('image', ImageType::class)
+            ->add('categories', EntityType::class, array(
+            'class' => 'CoreBundle:Category',
+            'choice_label' => 'name',
+            'multiple' => true,
+            'expanded' => true
+            ))
+             ->add('save',      SubmitType::class)
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CoreBundle\Entity\Article'
+        ));
+    }
+}
